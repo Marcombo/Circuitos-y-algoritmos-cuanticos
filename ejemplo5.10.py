0e27697125e2aed2e82a4371cbc1b3f0da990e64
JeanPierreDeschamps <71925902+JeanPierreDeschamps@users.noreply.github.com>
2023-01-15 10:38:47 +0100

    Delete ejemplo5.10.py

--- ejemplo5.10.py
+++
@@ -1,9 +0,0 @@
-import cirq
-c, t = cirq.LineQubit.range(2)
-puerta = cirq.Circuit(cirq.CZ(c, t))
-print(puerta)
-
-import cirq
-t, u = cirq.LineQubit.range(2)
-puerta = cirq.Circuit(cirq.SWAP(t, u))
-print(puerta)
